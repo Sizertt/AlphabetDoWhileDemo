f7849efb2761e03b98421a6ccd4d77d380149239
Sizertt <93645062+Sizertt@users.noreply.github.com>
2021-11-07 09:14:02 +0300

    AlphabetDoWhileDemo

---
+++ src/AlphabetDoWhileDemo.java
@@ -0,0 +1,15 @@
+public class AlphabetDoWhileDemo {
+    public static void main(String[] args) {
+
+        char ch = 'Я';
+
+        do {
+            System.out.println(ch);
+            ch--;
+
+        } while (ch >= 'А');
+
+    } // main(String[])
+} // AlphabetDoWhileDemo class
+
+
